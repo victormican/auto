30af5a9fd6179f1b5400391445dc92bf7a158a66
victormican <victormican@gmail.com>
2022-02-16 12:05:04 -0500

    Inicios Automatizacion

---
+++ src/test/java/features/test.feature
@@ -0,0 +1,7 @@
+Feature: Categoria Comics
+  Como usuario quiero leer chistes de testers para rerirme y pasar un buen rato.
+
+  Scenario: Ingresar a la categoria comics desde la pantalla Home
+    Given El usuario se encuentra en la pagina HOME de imalittletester
+    When Hace click sobre el boton The little tester comics
+    Then Se debe dirigir a la pantalla Comics
---
+++ src/test/java/seleniumgluecode/test.java
@@ -0,0 +1,24 @@
+package seleniumgluecode;
+
+import io.cucumber.java.en.*;
+
+
+public class test {
+
+    @Given("El usuario se encuentra en la pagina HOME de imalittletester")
+    public void el_usuario_se_encuentra_en_la_pagina_home_de_imalittletester() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("Hace click sobre el boton The little tester comics")
+    public void hace_click_sobre_el_boton_the_little_tester_comics() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("Se debe dirigir a la pantalla Comics")
+    public void se_debe_dirigir_a_la_pantalla_comics() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+}
